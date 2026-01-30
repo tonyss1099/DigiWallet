9ffdc6622f79fac17c4e8ebe9f3f45ee5a40667e
vksharma45 <bt22ece045@gmail.com>
2026-01-30 11:14:00 +0530

    ecx 3.st

--- src/main/java/com/orion/DigiWallet/service/CardService.java
+++ src/main/java/com/orion/DigiWallet/service/CardService.java
@@ -11,11 +11,19 @@ public class CardService {
 
     //TODO: 2.1.1
     // create a private final field for CardRepository (dependency)
-
-
+    private final CardRepository cardRepository;
 
     //TODO: 2.1.2
-    // create a constructor to inject CardRepository
+    //
+    public CardService(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
+    }
+
+
+
+
+//create a constructor to inject CardRepository
+
 
 
     //TODO: 2.1.3
@@ -25,12 +33,17 @@ public class CardService {
     // After completion of this method, you can test with swagger UI and using unit tests
     public Card createCard(Card card) {
 
+        if (cardRepository.existsByCardNumber(card.getCardNumber())) {
+            throw new RuntimeException("Card number already exists");
+        }
+
         // STEP 1: Check if card number already exists
         // to ensure uniqueness of card numbers in the system
         // throw runtime exception if it exists "Card number already exists"
 
         // STEP 2: Save and return the card
-        return null;
+        cardRepository.save(card);
+        return card;
     }
 
     //TODO: 2.1.4
@@ -41,8 +54,10 @@ public class CardService {
     public Card getCardById(Long id) {
 
         // STEP 1: Fetch card by ID
+        Card card = cardRepository.findById(id).orElseThrow(()-> new RuntimeException("Card not found with id: " + id));
+
         // throw runtime exception if not found "Card not found with id: " + id
-        return null;
+        return card;
     }
 
     //TODO: 2.1.5
@@ -54,14 +69,20 @@ public class CardService {
 
         // STEP 1: Fetch existing card
         // throw runtime exception if not found "Card not found with id: " + id
-       ;
+        Card card = cardRepository.findById(id).orElseThrow(()-> new RuntimeException("Card not found with id: " + id));
 
-        // STEP 2: Update allowed fields
+        // throw runtime exception if not found "Card not found with id: " + id
+        // STEP 2: Update allowed field
         // For simplicity, assume all fields except id and cardNumber can be updated
         // from updatedCard object get the values and set them to existingCard which you fetched in STEP 1
 
+        card.setExpiryDate(updatedCard.getExpiryDate());
+        card.setCardType(updatedCard.getCardType());
+        card.setStatus(updatedCard.getStatus());
+        card.setIssuedAt(updatedCard.getIssuedAt());
+        cardRepository.save(card);
         // STEP 3: Save updated card
-        return null;
+        return card;
     }
 
     //TODO: 2.1.6
@@ -73,6 +94,8 @@ public class CardService {
 
         // STEP 1: Check if card exists
         // throw runtime exception if not found "Card not found with id: " + id
+        Card card = cardRepository.findById(id).orElseThrow(()-> new RuntimeException("Card not found with id: " + id));
+        cardRepository.delete(card);
 
         // STEP 2: Delete card
 
